f57e9a75f625793b469be5e12f7802f9959c256d
Vijay <vijayvee@gmail.com>
2020-10-13 21:41:54 -0700

    Replacing ResNet v1net config

---
+++ models/resnet_v2_v1net_config.py
@@ -0,0 +1,17 @@
+"""Config file for ResNet_v2."""
+
+class ConfigDict(object):
+  pass
+
+def resnet_v2_config():
+  """Return configuration to build ResNet."""
+  config = ConfigDict()
+  config.image_size = 299
+  config.resnet_depth = 50
+  config.ckpt_dir = "pretrained_nets/resnet_v2_%s" % config.resnet_depth
+  config.num_classes = 1001
+  config.add_v1net = True
+  config.timesteps = 7
+  config.v1net_filters = 32
+  config.v1net_kernel_size = 5
+  return config
