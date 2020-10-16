6f56d3feda23fe52ad5a8d77d47e9be41eee6a30
Vijay <vijayvee@gmail.com>
2020-10-16 20:20:46 +0000

    Optimizer changes

--- optimizers.py
+++ optimizers.py
@@ -1,19 +1,20 @@
 """Create optimization functions."""
 import numpy as np
-import tensorflow as tf
+import tensorflow.compat.v1 as tf
+tf.disable_v2_behavior()
 
 def get_optimizer(learning_rate, opt, use_tpu=True):
   """Function to return optimizer instance."""
   if opt == 'adam':
-    optimizer = tf.compat.v1.train.AdamOptimizer(learning_rate=learning_rate)
+    optimizer = tf.train.AdamOptimizer(learning_rate=learning_rate)
   elif opt == 'rmsprop':
-    optimizer = tf.compat.v1.train.RMSPropOptimizer(learning_rate=learning_rate)
+    optimizer = tf.train.RMSPropOptimizer(learning_rate=learning_rate)
   elif opt == 'sgd':
-    optimizer = tf.compat.v1.train.GradientDescentOptimizer(learning_rate=learning_rate)
+    optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
   elif opt == 'momentum':
     optimizer = tf.train.MomentumOptimizer(learning_rate=learning_rate,
                                            momentum=0.9,
                                            use_nesterov=True)
   if use_tpu:
-    optimizer = tf.contrib.tpu.CrossShardOptimizer(optimizer)
+    optimizer = tf.tpu.CrossShardOptimizer(optimizer)
   return optimizer
