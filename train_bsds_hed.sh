0835635c06bf25975fc7459bc85e4bf0fcaa5596
Vijay <vijayvee@gmail.com>
2020-10-25 05:27:59 +0000

    BSDS latest training

--- train_bsds_hed.sh
+++ train_bsds_hed.sh
@@ -3,16 +3,16 @@ export PYTHONPATH=$PYTHONPATH:/home/vveeraba/src/recurrent_vision:/home/vveeraba
 
 LEARNING_RATE=1e-5
 WEIGHT_DECAY=1e-4
-NUM_EPOCHS=100
+NUM_EPOCHS=50
 TRAIN_BATCH_SIZE=8
 EVAL_BATCH_SIZE=8
-EVALUATE_EVERY=2
-EXPERIMENT_NAME="bsds_vgg_hed_training_ft"
+EXPERIMENT_NAME="bsds_vgg_hed_training_ft_fin"
 CHECKPOINT="gs://v1net-tpu-bucket/checkpoints/vgg_16/vgg_16.ckpt"
 OPTIMIZER="adam"
 USE_TPU=True
+ADD_V1NET_EARLY=True
 TPU_NAME=$1
-DATA_DIR="bsds_data/bsds500"
+DATA_DIR="bsds_data/HED-BSDS/tfrecords"
 
 echo "Running on {$1}"
 python bsds_main.py \
@@ -21,10 +21,10 @@ python bsds_main.py \
 	--num_epochs=${NUM_EPOCHS} \
 	--train_batch_size=${TRAIN_BATCH_SIZE} \
 	--eval_batch_size=${EVAL_BATCH_SIZE} \
-	--evaluate_every=${EVALUATE_EVERY} \
 	--experiment_name=${EXPERIMENT_NAME} \
 	--checkpoint=${CHECKPOINT} \
 	--optimizer=${OPTIMIZER} \
 	--use_tpu=${USE_TPU} \
+	--add_v1net_early=${ADD_V1NET_EARLY} \
 	--tpu_name=${TPU_NAME} \
 	--data_dir=${DATA_DIR}
